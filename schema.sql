4da4624a78441f1d7ef32c8b20d408c5263593e5
EC2 Default User <ec2-user@ip-172-31-19-64.us-east-2.compute.internal>
2020-04-27 01:26:52 +0000

    adding Dockerfile and schema.sql

---
+++ schema.sql
@@ -0,0 +1,9 @@
+create table products(
+	prod_id integer primary key,
+	prod_name varchar,
+	prod_qty integer
+);
+
+insert into products values (1,'butter',8);
+insert into products values (2,'milk',7);
+insert into products values (3,'bread',10);
